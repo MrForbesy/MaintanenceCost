cd758feece0c400a1e996731f3e98bf4a2666602
Forbesy <mvforbes21@gmail.com>
2023-06-02 22:57:10 -0400

    Initial commit

---
+++ src/YearlyMaintenanceCost.java
@@ -0,0 +1,14 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double springCost = 3230.75;
+        double summerCost = 5235.98;
+        double fallCost = 2343.42;
+        double winterCost = 6001.22;
+        double totalCost = springCost + summerCost + fallCost + winterCost;
+
+        System.out.println("Your Spring maintenance cost is " + springCost  + ". Your Summer maintenance cost is " + summerCost + ". Your Fall maintenance cost is " + fallCost + ". Your total maintenance cost for this year is " + totalCost + ".");
+
+    }
+}
